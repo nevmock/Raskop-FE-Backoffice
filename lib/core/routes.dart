833596200cdb21b8a095d7af3bb04aab64bc3b22
krisnaganesha1609 <igkganes@gmail.com>
2024-12-11 23:05:18 +0700

    RSKPXX | UPDATE APP ROUTES | APP ROUTES

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -7,15 +7,15 @@ import 'package:raskop_fe_backoffice/src/common/home_screen.dart';
 /// MAIN ROUTER
 final router = GoRouter(
   routes: [
-    // Masih route template, nanti setelah bagi-bagi tiket kita ganti okeh!
     GoRoute(
-      path: '/${MyHomePage.route}',
-      name: MyHomePage.route,
-      builder: (context, state) => const MyHomePage(title: AppStrings.appName),
+      path: '/${HomeScreen.route}',
+      name: HomeScreen.route,
+      builder: (context, state) => const HomeScreen(title: AppStrings.appName),
     ),
   ],
+  initialLocation: '/${HomeScreen.route}',
   redirect: (context, state) {
-    return '/${MyHomePage.route}';
+    return '/${HomeScreen.route}';
   },
   observers: [
     routeObserver,
