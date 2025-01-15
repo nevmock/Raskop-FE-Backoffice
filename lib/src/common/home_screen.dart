82a37f1aada972cd2031fcf48e6cbff3334dea47
krisnaganesha1609 <igkganes@gmail.com>
2025-01-15 12:23:53 +0700

    RSKP-7 | Finish UI CRUD Order | Order

--- lib/src/common/home_screen.dart
+++ lib/src/common/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:raskop_fe_backoffice/src/common/widgets/sidebar_widget.dart';
 import 'package:raskop_fe_backoffice/src/menu/presentation/screens/menu_screen.dart';
+import 'package:raskop_fe_backoffice/src/order/presentation/screens/order_screen.dart';
 import 'package:raskop_fe_backoffice/src/supplier/presentation/screens/supplier_screen.dart';
 
 /// Home Page
@@ -23,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   // URUTAN INDEX: 0. DASHBOARD, 1. ORDER, 2. MENU, 3. RESERVATION, 4. TABLE, 5. SUPPLIER
   List<Widget> screens = [
     //const DashboardScreen(),
-    //const OrderScreen(),
+    const OrderScreen(),
     const MenuScreen(),
     //const ReservationScreen(),
     //const TableScreen(),
@@ -35,6 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.white,
         body: Row(
           children: [
             SizedBox(
---
+++ lib/src/order/presentation/widgets/group_button_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:raskop_fe_backoffice/shared/const.dart';
+
+///
+class GroupButton extends StatefulWidget {
+  ///
+  const GroupButton({
+    required this.isSelected,
+    required this.onPressed,
+    required this.borderRadius,
+    required this.children,
+    super.key,
+  });
+
+  ///
+  final BorderRadiusGeometry? borderRadius;
+
+  ///
+  final List<bool> isSelected;
+
+  ///
+  final List<Widget> children;
+
+  ///
+  final void Function(int idx) onPressed;
+
+  @override
+  State<GroupButton> createState() => _GroupButtonState();
+}
+
+class _GroupButtonState extends State<GroupButton> {
+  List<bool> get isSelected => widget.isSelected;
+
+  void Function(int idx) get onPressed => widget.onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final btn = List<Widget>.generate(widget.children.length, (int idx) {
+      return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 2.w),
+        child: ClipRRect(
+          borderRadius: widget.borderRadius ?? BorderRadius.zero,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: isSelected[idx]
+                  ? hexToColor('#1F4940')
+                  : hexToColor('#CACACA'),
+              visualDensity: VisualDensity.standard,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              animationDuration: kThemeChangeDuration,
+              enableFeedback: true,
+              alignment: Alignment.center,
+              splashFactory: InkRipple.splashFactory,
+            ),
+            onPressed: () {
+              onPressed(idx);
+            },
+            child: widget.children[idx],
+          ),
+        ),
+      );
+    });
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: btn,
+      ),
+    );
+  }
+}
