3d7d2c70a25b5e61a332dd15b7c975c35d250be5
krisnaganesha1609 <igkganes@gmail.com>
2025-01-28 14:37:24 +0700

    RSKP | Added device_preview packages and disabled ScreenUtil scaling | App

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+// ignore_for_file: avoid_redundant_argument_values
+
+import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:raskop_fe_backoffice/core/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: RasKopBackOfficeApp()));
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode,
+      builder: (ctx) => const ProviderScope(child: RasKopBackOfficeApp()),
+    ),
+  );
 }
