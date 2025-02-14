2a71a092165f297f38e0c36166d12f15747e85e4
krisnaganesha1609 <igkganes@gmail.com>
2025-02-14 13:20:12 +0700

    RSKP | added generic type api client | Core

---
+++ lib/core/client.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:raskop_fe_backoffice/core/core.dart';
+import 'package:raskop_fe_backoffice/res/paths.dart';
+
+/// http provider
+final httpClientProvider = Provider<http.Client>((ref) {
+  final client = http.Client();
+
+  ref.onDispose(client.close);
+
+  return client;
+});
+
+/// api client provier
+final apiClientProvider = Provider<ApiClient>((ref) {
+  final client = ref.watch(httpClientProvider);
+  return ApiClient(client: client, baseUrl: BasePaths.baseAPIURL);
+});
